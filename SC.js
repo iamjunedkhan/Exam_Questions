6fdfcd57bc542904b0482455719deac5a016bbd7
Juned <jund7734@gmail.com>
2022-11-23 11:28:11 +0530

    m

--- SC.js
+++ SC.js
@@ -1,71 +1,75 @@
 let softComputing = [
-    {
-      title: "Exaplain Mcculloch, Pitt's neuron model with example",
-      link: "https://www.geeksforgeeks.org/implementing-models-of-artificial-neural-network/",
-    },
-    {
-      title: "Explain Comparison of ANN and Biological NN?",
-      link: "https://www.geeksforgeeks.org/difference-between-ann-and-bnn/",
-    },
-    {
-      title: "Explain the type of learning?",
-      link: "https://www.javatpoint.com/types-of-machine-learning",
-    },
-    {
-      title: "Discuss the application of neural network",
-      link: "https://www.geeksforgeeks.org/artificial-neural-networks-and-its-applications/",
-    },
-    {
-      title: "Explain the Linear Separability of neural networks?",
-      link: "https://intellipaat.com/community/11269/neural-networks-what-does-linearly-separable-mean",
-    },
-    {
-      title:
-        "Define different steps involved in the trainig algorithm perception",
-      link: "https://www.tutorialspoint.com/artificial_neural_network/artificial_neural_network_supervised_learning.htm",
-    },
-    {
-      title: "Explain the Adaptive Linear Neurons (Adaline) &  Multiple Adaptive Linear Neurons (Madaline)",
-      link: "https://www.tutorialspoint.com/artificial_neural_network/artificial_neural_network_supervised_learning.htm",
-    },
-    {
-      title: "Explain the architecture  and training algorithm of Back Propagation network?",
-      link: "https://www.geeksforgeeks.org/backpropagation-in-data-mining/",
-    }
-    ,{
-      title:'Explain 1 to convolutional neural networks (CNN)',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html',
-    }
-    ,{
-      title:'Explain Recurrent neural netwokrs (RNN)',
-      link:'https://www.geeksforgeeks.org/introduction-to-recurrent-neural-network/'
-    }
-    ,{
-      title:'Explain Fixed weight competitve nets',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    }
-    ,{
-      title:'Discuss counter Propagation networks',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    },{
-      title:'Discuss the application of neural networks',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    },{
-      title:'Define fuzzy set Terminology and properties',
-      link:'https://www.geeksforgeeks.org/fuzzy-logic-set-2-classical-fuzzy-sets/'
-    },{
-      title:'Explain Fuzzy rules and fuzzy reasoning. Fuzzy if-then rules, fuzzy Inference Systems.s',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    },{
-      title:'Explain Application of Fuzzy logic in solving engineering problems',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    },
-    {
-      title:'Explain genetic algorithm with terminologies and operators',
-      link:'https://www.geeksforgeeks.org/genetic-algorithms/'
-    },
-    {
-      title:'Genetic optimization techniques like Travelling salesman usin genetic algorithm',
-      link:'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
-    }
-  ];
+  {
+    title: "Exaplain Mcculloch, Pitt's neuron model with example",
+    link: "https://www.geeksforgeeks.org/implementing-models-of-artificial-neural-network/",
+  },
+  {
+    title: "Explain Comparison of ANN and Biological NN?",
+    link: "https://www.geeksforgeeks.org/difference-between-ann-and-bnn/",
+  },
+  {
+    title: "Explain the type of learning?",
+    link: "https://www.javatpoint.com/types-of-machine-learning",
+  },
+  {
+    title: "Discuss the application of neural network",
+    link: "https://www.geeksforgeeks.org/artificial-neural-networks-and-its-applications/",
+  },
+  {
+    title: "Explain the Linear Separability of neural networks?",
+    link: "https://intellipaat.com/community/11269/neural-networks-what-does-linearly-separable-mean",
+  },
+  {
+    title:
+      "Define different steps involved in the trainig algorithm perception",
+    link: "https://www.tutorialspoint.com/artificial_neural_network/artificial_neural_network_supervised_learning.htm",
+  },
+  {
+    title: "Explain the Adaptive Linear Neurons (Adaline) &  Multiple Adaptive Linear Neurons (Madaline)",
+    link: "https://www.tutorialspoint.com/artificial_neural_network/artificial_neural_network_supervised_learning.htm",
+  },
+  {
+    title: "Explain the architecture  and training algorithm of Back Propagation network?",
+    link: "https://www.geeksforgeeks.org/backpropagation-in-data-mining/",
+  },
+  {
+    title: 'Explain 1 to convolutional neural networks (CNN)',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html',
+  },
+  {
+    title: 'Explain Recurrent neural netwokrs (RNN)',
+    link: 'https://www.geeksforgeeks.org/introduction-to-recurrent-neural-network/'
+  },
+  {
+    title: 'Explain Fixed weight competitve nets',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  },
+  {
+    title: 'Discuss counter Propagation networks',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  },
+  {
+    title: 'Discuss the application of neural networks',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  },
+  {
+    title: 'Define fuzzy set Terminology and properties',
+    link: 'https://www.geeksforgeeks.org/fuzzy-logic-set-2-classical-fuzzy-sets/'
+  },
+  {
+    title: 'Explain Fuzzy rules and fuzzy reasoning. Fuzzy if-then rules, fuzzy Inference Systems.s',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  },
+  {
+    title: 'Explain Application of Fuzzy logic in solving engineering problems',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  },
+  {
+    title: 'Explain genetic algorithm with terminologies and operators',
+    link: 'https://www.geeksforgeeks.org/genetic-algorithms/'
+  },
+  {
+    title: 'Genetic optimization techniques like Travelling salesman usin genetic algorithm',
+    link: 'https://iamjunedkhan.github.io/Exam_Questions/nf.html'
+  }
+];  
