ae72520aa8900ec71b8a3bd0df92c706ed38bb20
Juned <jund7734@gmail.com>
2022-11-26 14:59:18 +0530

    m

---
+++ CC.js
@@ -0,0 +1,87 @@
+let cloudComputing = [
+    {
+      title: "What is Cloud Computing?",
+      link: "https://www.investopedia.com/terms/c/cloud-computing.asp",
+    },
+    {
+      title: "Explain Characterstics of cloud as per NIST?",
+      link: "https://easyexamnotes.com/characteristics-of-cloud-computing-as/",
+    },
+    {
+      title: "Define cloud compuiting reference model ?",
+      link: "https://www.knowledgehut.com/blog/cloud-computing/cloud-computing-reference-model",
+    },
+    {
+      title: "What is ECG anaylisis in cloud? Explain",
+      link: "https://www.slideshare.net/golujain/ecg-analysis-in-the-cloud",
+    },
+    {
+      title: "Applications of Cloud Computing (1)",
+      link: "https://www.interviewbit.com/blog/applications-of-cloud-computing/",
+    },
+    {
+      title: "Applications of Cloud Computing (2)",
+      link: "https://www.simplilearn.com/applications-of-cloud-computing-article",
+    },
+    {
+      title: "Define role of cloud in social networking ?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title:
+        "Explain various types of services provided by cloud computing paradigm?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title: "Explain the concept of fault tolerence in cloud?",
+      link: "https://www.javatpoint.com/fault-tolerance-in-cloud-computing",
+    },
+    {
+      title: "write short note on: Cloud Ecosystem",
+      link: "https://easyexamnotes.com/cloud-ecosystem/",
+    },
+    {
+      title: "write short note on: Cloud Analytics",
+      link: "https://www.splunk.com/en_us/data-insider/what-is-cloud-analytics.html",
+    },
+    {
+      title:
+        "Write the difference between block level & file Level storage virtualization?",
+      link: "https://www.ques10.com/p/20616/explain-block-level-and-file-level-storage-virtu-1/",
+    },
+    {
+      title:
+        "Describe different types of hypervisors with example, and block diagram ? ",
+      link: "https://www.geeksforgeeks.org/hypervisor/",
+    },
+    {
+      title: "Enlist the advantages of virtualization?",
+      link: "https://cloudlytics.com/virtualization-gains-popularity-as-a-viable-solution-for-enhancing-cloud-security/",
+    },
+    {
+      title:
+        "Exaplain Map Reduce Methodology. How it helps in implementeing complex problems in cloud computing?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title: "write the difference between VLAN & VSAN?",
+      link: "https://www.slideshare.net/neeleshcmc/introduction-of-vlan-and-vsan-with-its-benefits",
+    },
+    {
+      title: "how securitypolicies are implemented in cloud computing?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title: "explain cloud computing security architecture?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title:
+        "explain the cloud security fundamentals introduced in cloud computing?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+    {
+      title: "explain virtualization security management in cloud computing?",
+      link: "https://iamjunedkhan.github.io/Exam_Questions/nf.html",
+    },
+  ];
